5ef91f040fc7d6b09c41d30dac0eb713c4315741
407 <g4x0v7@gmail.com>
2022-02-21 08:01:39 +1300

    Updates

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,22 @@
-# Container image that runs your code
 FROM alpine:3.15
 
-RUN apk add --no-cache ca-certificates git gnupg jq
-# Copies your code file from your action repository to the filesystem path `/` of the container
-COPY entrypoint.sh /entrypoint.sh
+RUN apk add --no-cache \
+    ca-certificates=20211220-r0 \
+    gawk=5.1.1-r0 \
+    git=2.34.1-r0 \
+    gnupg=2.2.31-r1 \
+    jo=1.4-r0 \
+    jq=1.6-r1
+    COPY entrypoint.sh /entrypoint.sh
 RUN ["chmod", "+x", "/entrypoint.sh"]
+# hadolint ignore=DL4006
+RUN wget https://github.com/mikefarah/yq/releases/download/v4.20.2/yq_linux_amd64 -O /usr/bin/yq &&\
+    chmod +x /usr/bin/yq
+RUN wget -q -O - https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sh -s -- -b /opt/bin/trivy \
+    && export PATH=$PATH:/opt/bin/trivy \
+    && trivy filesystem --skip-dirs /opt/bin/trivy --exit-code 1 --no-progress / \
+    && trivy image --reset \
+    && rm -rf /opt/bin/trivy \
+    && rm -rf /root/.cache
 
-# Code file to execute when the docker container starts up (`entrypoint.sh`)
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -15,6 +15,18 @@ _print_actions_debug "got INPUT_PUBKEY: ${INPUT_PUBKEY}"
 _print_actions_debug "got INPUT_PUBKEY_URL: ${INPUT_PUBKEY_URL}"
 _print_actions_debug "got INPUT_PUBKEY_URL_HASH: ${INPUT_PUBKEY_URL_HASH}"
 
+_print_versions() {
+  jo type=debug \
+    name=versions \
+    os="$(cat /etc/os-release)" \
+    tool[gpg]="$(gpg --version | head -2)" \
+    tool[git]="$(git --version)" \
+    tool[jq]="$(jq --version)" \
+    tool[yq]="$(yq --version)" \
+    tool[jo]="$(jo -V)"
+}
+_print_versions | jq | yq e -P '.' -
+
 _actions_start_group() {
   echo "::group::$1"
   echo "$2"
@@ -24,13 +36,13 @@ _actions_start_group() {
 # argparsing logic
 _has_commit_or_tag() {
   if [ -n "${INPUT_REF}" ] && [ -n "${INPUT_TAG}" ]; then
-    echo '{"func": "_has_commit_or_tag()","exit_code": 1,"msg": "ERR:  Please specify a ref, or a tag, but not both"}'
+    echo '{"func": "_has_commit_or_tag()","exit_code": 1,"msg": "ERR:  Please specify a ref, or a tag, but not both"}' | jq
   elif [ -z "${INPUT_REF}" ] && [ -n "${INPUT_TAG}" ]; then
-    echo '{"type": "tag","ref": "","exit_code": 0,"msg": ""}' | jq -r ".ref |= \"$INPUT_TAG\""
+    echo '{"type": "tag","ref": "","exit_code": 0,"msg": ""}' | jq -r ".ref |= \"$INPUT_TAG\"" | jq
   elif [ -n "${INPUT_REF}" ] && [ -z "${INPUT_TAG}" ]; then
-    echo '{"type": "commit","ref": "","exit_code": 0,"msg": ""}' | jq -r ".ref |= \"$INPUT_REF\""
+    echo '{"type": "commit","ref": "","exit_code": 0,"msg": ""}' | jq -r ".ref |= \"$INPUT_REF\"" | jq
   else
-    echo '{"func": "_has_commit_or_tag()","exit_code": 1,"msg": "ERR:  Please specify a ref, or a tag"}'
+    echo '{"func": "_has_commit_or_tag()","exit_code": 1,"msg": "ERR:  Please specify a ref, or a tag"}' | jq
   fi
 }
 
@@ -39,11 +51,19 @@ _has_pubkey_or_url() {
     echo '{"func": "_has_pubkey_or_url()","exit_code": 1,"msg": "ERR:  Please specify a public key, or a url, but not both"}'
     # exit 1
   elif [ -z "${INPUT_PUBKEY}" ] && [ -n "${INPUT_PUBKEY_URL}" ]; then
-    echo '{"type": "url","exit_code": 0,"msg": ""}'
+    # echo '{"type": "url","exit_code": 0,"msg": ""}' 
+    jo type=url \
+      exit_code=0 \
+      msg=""
   elif [ -n "${INPUT_PUBKEY}" ] && [ -z "${INPUT_PUBKEY_URL}" ]; then
-    echo '{"type": "key","exit_code": 0,"msg": ""}'
+    # echo '{"type": "key","exit_code": 0,"msg": ""}'
+    jo type=key \
+      exit_code=0 \
+      msg=""
   else
-    echo '{"func": "_has_pubkey_or_url()","exit_code": 1,"msg": "ERR:  Please specify a public key, or a url"}'
+    jo func='_has_pubkey_or_url()' \
+      exit_code=1 \
+      ERR="ERR:  Please specify a public key, or a url"
   fi
 }
 
@@ -73,21 +93,29 @@ _verify_commit() {
     if result=$(git verify-commit "${INPUT_REF}" --raw 2>&1); then
       echo "Verify commit successful"
       VERIFY_SUCCESS="$result"
+      jo -p func='_verify_commit()' \
+        type=result \
+        exit_code=0 \
+        msg="$result" |  yq e '... style="" | .msg style="literal"' -
       _parse_success_message
       SIGNED_BOOL='true'
       echo "::set-output name=signed::${SIGNED_BOOL}"
     else
-      echo "Verify commit failed" >&2
-      ERROR=$(echo "$result" | tail -5 | sed --expression='s/\"/\\"/g') # escape any quotes for json
-      echo '{"func": "_verify_commit()","exit_code": 1,"msg": "ERR:  =--"}' | jq -r ".msg |= \"ERR:  $ERROR\""
+      echo "Verify commit failed"
+      jo  func='_verify_commit()' \
+        type=result \
+        exit_code=1 \
+        msg="$result" | jq -r| yq e '... style="" | .msg style="literal"' -
       SIGNED_BOOL='false'
       echo "::set-output name=signed::${SIGNED_BOOL}"
     fi
 }
 
 _verify_tag() {
-    if result=$(git verify-tag "${INPUT_TAG}" 2>&1); then
+    if result=$(git verify-tag "${INPUT_TAG}" --raw 2>&1); then
       echo "Verify tag successful"
+      VERIFY_SUCCESS="$result"
+      _parse_success_message
       SIGNED_BOOL='true'
       echo "::set-output name=signed::${SIGNED_BOOL}"
     else
@@ -133,47 +161,195 @@ _gpg_permissions_fix() {
 _init_gpg_config() {
   _gpg_permissions_fix
   mkdir -p ~/.gnupg
-  touch ~/.gnupg/pubring.kbx
-  echo "keyserver hkp://pgp.rediris.es" >> ~/.gnupg/gpg.conf # works, but why? loop afew ks maybe
+  touch ~/.gnupg/trustdb.gpg
+  touch ~/.gnupg/pubring.kbx # newer default syntax in some versions of gnupg
+  echo "keyserver hkp://pgp.rediris.es" >> ~/.gnupg/gpg.conf # works, but why? loop afew ks maybecat 
+  # echo "keyserver hkps://keyserver.ubuntu.com" >> ~/.gnupg/gpg.conf
   # echo "keyserver hkps://keys.openpgp.org" >> ~/.gnupg/gpg.conf
+  # echo "keyserver hkp://keys.gnupg.net " >> ~/.gnupg/gpg.conf
+  # echo "keyserver hkps://pgp.surf.nl" >> ~/.gnupg/gpg.conf
   # echo "keyserver hkp://pgp.mit.edu" >> ~/.gnupg/gpg.conf
 }
 
+_opengpg_api_getkey() {
+  if [ "$(echo ${#INPUT_PUBKEY})" = 16 ]; then
+    echo "got a long key id"
+    wget -qO- "https://keys.openpgp.org/vks/v1/by-keyid/$INPUT_PUBKEY" | gpg --import
+  else
+    echo "got a key fingerprint"
+    wget -qO- "https://keys.openpgp.org/vks/v1/by-fingerprint/$INPUT_PUBKEY" | gpg --import
+  fi
+}
+
+
 _import_from_keyserver() {
   echo "Importing from keyserver"
-  _init_gpg_config
-  if result=$(gpg --batch --recv "${INPUT_PUBKEY}" 2>&1); then
+  # _init_gpg_config
+  # if result=$(_opengpg_api_getkey); then
+  if result=$(gpg --batch --recv-key --yes "${INPUT_PUBKEY}" 2>&1); then
     echo "Import successful"
+    jo -p func='_import_from_keyserver()' \
+      type=result \
+      msg="$result" | jq | yq e -P '.' -
   else
     echo "Import failed"
 
     ERROR=$(echo "$result" | tail -5 | sed --expression='s/\"/\\"/g') # escape any quotes for json
-    echo '{"func": "_import_from_keyserver()","exit_code": 1,"msg": "ERR:  =--"}' | jq -r ".msg |= \"ERR:  $ERROR\"" | jq -r
+    echo '{"func": "_import_from_keyserver()","exit_code": 1,"msg": "ERR:  =--"}' | jq -r ".msg |= \"ERR:  $ERROR\"" | jq -r \
+      | yq e -P '.' -
     exit 1
   fi
 }
 
 _import_from_url() {
   echo "Importing from url"
-  _init_gpg_config
+  # _init_gpg_config
   if result=$( { wget -qO- "${INPUT_PUBKEY_URL}" | gpg --import; } 2>&1 ); then
     echo "Import successful"
+    jo -p func='_import_from_keyserver()' \
+      type=result \
+      msg="$result" | jq | yq e -P '.' -
   else
     echo "Import failed"
     ERROR=$(echo "$result" | tail -5 | sed --expression='s/\"/\\"/g') # escape any quotes for json
-    echo '{"func": "_import_from_url()","exit_code": 1,"msg": "ERR:  =--"}' | jq -r ".msg |= \"ERR:  $ERROR\"" | jq -r
+    echo '{"func": "_import_from_url()","exit_code": 1,"msg": "ERR:  =--"}' | jq -r ".msg |= \"ERR:  $ERROR\"" | jq -r | yq e -P '.' -
     exit 1
   fi
 }
 
+_list_fingerprints() {
+  __runcmd='gpg --list-keys --fingerprint --with-colons'
+
+  if __cmd=$(eval "$__runcmd" 2>&1); then
+  jo func='_list_fingerprints()' \
+    type=result \
+    command="$__runcmd"  \
+    internal=true \
+    no_yq_pretty=true \
+    exit_code=0 \
+    stdout="$__cmd"
+  fi
+
+}
+
+
+
+    # if result=$(git verify-commit "${INPUT_REF}" --raw 2>&1); then
+    #   echo "Verify commit successful"
+    #   VERIFY_SUCCESS="$result"
+    #   jo -p func='_verify_commit()' \
+    #     type=result \
+    #     exit_code=1 \
+    #     msg="$result" | jq | yq e -P '.' -
+
+
 _set_key_trust() {
   echo "Setting key trust"
-  gpg --list-keys --fingerprint --with-colons 2>&1 | grep scSC | awk -F: '{print "trusted-key "$5}' >> ~/.gnupg/gpg.conf
+
+  # List fingerprints, capture result
+  if result=$(_list_fingerprints); then
+    jo -p func='_set_key_trust()' \
+      type=result \
+      called_cmd='_list_fingerprints' \
+      exit_code=0 \
+      result="$result" \
+      stdout="$(echo $result | yq e '.stdout style="literal"' - | jq -r .stdout)"  | yq e -P '.' -
+  fi
+
+      # Echo key to set ultimate trust
+      # echo "$result"
+      # echo "$result" | yq e '.stdout style="literal"' - | jq -r .stdout
+
+      # echo "$result" #| yq e '.result.stdout style="literal"' - #| jq -r .stdout
+      
+
+      # echo $result | yq e '... style="" | .stdout style="literal"' -
+      # echo -n $result | yq e -P '.stdout' -
+      # __DEBUGSTOP
+      echo "$result" | jq -r .stdout | grep pub | awk -F: '{print "trusted-key "$5}' >> ~/.gnupg/gpg.conf
+      # __runcmd='echo "$result" | jq -r .msg | grep pub | awk -F: '\''{print "trusted-key "$5}'\'' >> ~/.gnupg/gpg.conf'
+
+      jo func='_set_key_trust()' \
+        type=result \
+        filecontent='~/.gnupg/gpg.conf' \
+        msg="$(cat  /root/.gnupg/gpg.conf)" | yq e -P '.' -
+        # msg="$(cat ~/.gnupg/gpg.conf 2>&1)" | jq -r | yq e -P '.' -
+
+      # if __cmd=$(eval "$__runcmd"); then
+      # jo func='_set_key_trust()' \
+      #   type=result \
+      #   command="$__runcmd"  \
+      #   internal=true \
+      #   no_yq_pretty=true \
+      #   msg="$__cmd" | yq e -P '.' -
+      # fi
+
+      # jo func='_set_key_trust()' \
+      #   type=result \
+      #   cmd='set_ultimate_trust___' \
+      #   msg="$set_ult" | jq -r | yq e -P '.' -
+
+  # __runcmd='gpg --list-keys --fingerprint --with-colons 2>&1'
+
+  # if __cmd=$(eval "$__runcmd"); then
+  # jo func='_list_fingerprints()' \
+  #   type=result \
+  #   command="$__runcmd"  \
+  #   internal=true \
+  #   no_yq_pretty=true \
+  #   msg="$__cmd"
+  # fi
+
+
+    # filecontent='~/.gnupg/gpg.conf' \
+  #     type=result \
+  #     exit_code=0 \
+  #     msg="$result" | yq e -P '.' -
+  # echo $result | yq e -P '.' -
+  # IMPORTED_KEYS=$(gpg --list-keys --fingerprint --with-colons 2>&1)
+    # msg="$(cat ~/.gnupg/gpg.conf 2>&1)" | jq -r | yq e -P '.' -
+
+  # will show ultimate trust, as this cmd is run after first listing the keys
+  # jo func='_set_key_trust()' \
+  #   type=keysfpr \
+  #   msg="$(gpg --list-keys --fingerprint)" | yq e -P '.' -
+
+
+  # jo func='_set_key_trust()' \
+  #   type=keys \
+  #   msg="$IMPORTED_KEYS" | yq e -P '.' -
 }
 
 _update_trustdb() {
   echo "Updating trustdb"
-  gpg --update-trustdb 2>&1 | grep 'gpg: key'
+  __runcmd='gpg --update-trustdb 2>&1'
+
+  if __cmd=$(eval "$__runcmd" 2>&1); then
+  jo func='_update_trustdb()' \
+    type=result \
+    command="$__runcmd"  \
+    internal=true \
+    no_yq_pretty=false \
+    exit_code=0 \
+    stdout="$__cmd" \
+    | jq | yq e -P '.' -
+  fi
+}
+
+_list_keys() {
+  echo "Listing keys"
+  __runcmd='gpg --list-keys --fingerprint'
+
+  if __cmd=$(eval "$__runcmd" 2>&1); then
+  jo func='_list_keys()' \
+    type=result \
+    command="$__runcmd"  \
+    internal=true \
+    no_yq_pretty=false \
+    exit_code=0 \
+    stdout="$__cmd" \
+    | jq | yq e -P '.' -
+  fi
 }
 
 _import_pubkey() {
@@ -184,11 +360,13 @@ _import_pubkey() {
     _import_from_keyserver
     _set_key_trust
     _update_trustdb
+    _list_keys
   elif [ "$SIGN_KEY" = "url" ]; then
     echo "got a url"
     _import_from_url
     _set_key_trust
     _update_trustdb
+    _list_keys
   fi
 }
 
